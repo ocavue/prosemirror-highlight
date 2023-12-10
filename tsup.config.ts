7b54573a505939921db62d6d565dcd23a2acacb3
ocavue <ocavue@gmail.com>
2023-12-11 00:48:55 +0800

    add build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/lowlight.ts', 'src/shiki.ts', 'src/shikiji.ts'],
+  entry: [
+    'src/index.ts',
+    'src/lowlight.ts',
+    'src/refractor.ts',
+    'src/shiki.ts',
+    'src/shikiji.ts',
+  ],
   format: ['esm'],
   clean: true,
   dts: true,
