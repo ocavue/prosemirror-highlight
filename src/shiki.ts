ea2d87a97a4c556a52453d19162fbe44ada22098
ocavue <ocavue@gmail.com>
2023-12-16 11:01:51 +0800

    fix: export type Parser

--- src/shiki.ts
+++ src/shiki.ts
@@ -3,6 +3,8 @@ import { type Highlighter } from 'shiki'
 
 import type { Parser } from './types'
 
+export type { Parser }
+
 export function createParser(highlighter: Highlighter): Parser {
   return function parser({ content, language, pos }) {
     const decorations: Decoration[] = []
