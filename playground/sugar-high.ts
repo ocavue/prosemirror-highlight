ca5382b15d12c790894f2f8052f55fd983f6e930
ocavue <ocavue@gmail.com>
2024-05-21 00:40:18 +1000

    format

--- playground/sugar-high.ts
+++ playground/sugar-high.ts
@@ -1,6 +1,5 @@
-import { createParser } from 'prosemirror-highlight/sugar-high'
-
 import { createHighlightPlugin } from 'prosemirror-highlight'
+import { createParser } from 'prosemirror-highlight/sugar-high'
 
 const parser = createParser()
 export const sugarHighPlugin = createHighlightPlugin({ parser })
