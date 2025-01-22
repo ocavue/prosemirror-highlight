7225bb9cf7089aa386595301d3cb252ff3146a97
ocavue <ocavue@gmail.com>
2025-01-22 23:09:20 +1100

    docs: fix refractor example

--- playground/refractor.ts
+++ playground/refractor.ts
@@ -1,6 +1,6 @@
 import { createHighlightPlugin } from 'prosemirror-highlight'
 import { createParser } from 'prosemirror-highlight/refractor'
-import { refractor } from 'refractor'
+import { refractor } from 'refractor/lib/all'
 
 const parser = createParser(refractor)
 export const refractorPlugin = createHighlightPlugin({ parser })
