4ea7c11b3734d953dac6ecc1a2224a253fee059b
ocavue <ocavue@gmail.com>
2025-03-14 15:21:58 +1100

    ci: add pkg-pr-new

--- playground/lowlight.ts
+++ playground/lowlight.ts
@@ -1,6 +1,7 @@
 import 'highlight.js/styles/default.css'
 
 import { common, createLowlight } from 'lowlight'
+
 import { createHighlightPlugin } from 'prosemirror-highlight'
 import { createParser } from 'prosemirror-highlight/lowlight'
 
--- playground/shiki.ts
+++ playground/shiki.ts
@@ -1,6 +1,7 @@
+import { getSingletonHighlighter } from 'shiki'
+
 import { createHighlightPlugin } from 'prosemirror-highlight'
 import { createParser } from 'prosemirror-highlight/shiki'
-import { getSingletonHighlighter } from 'shiki'
 
 const highlighter = await getSingletonHighlighter({
   themes: ['github-light'],
