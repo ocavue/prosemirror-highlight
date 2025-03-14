141548139a4fab4ed73d8057104a3d77cb8bc8d5
ocavue <ocavue@users.noreply.github.com>
2025-03-14 15:24:15 +1100

    ci: add pkg-pr-new (#85)

--- playground/refractor.ts
+++ playground/refractor.ts
@@ -1,6 +1,7 @@
+import { refractor } from 'refractor/lib/all'
+
 import { createHighlightPlugin } from 'prosemirror-highlight'
 import { createParser } from 'prosemirror-highlight/refractor'
-import { refractor } from 'refractor/lib/all'
 
 const parser = createParser(refractor)
 export const refractorPlugin = createHighlightPlugin({ parser })
