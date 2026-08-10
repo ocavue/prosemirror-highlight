fbfaf43c6e28833ac45e622698a165227b343a34
ocavue <ocavue@gmail.com>
2026-08-10 11:47:03 +1000

    fix

--- src/sugar-high.ts
+++ src/sugar-high.ts
@@ -1,17 +1,22 @@
 import { Decoration } from 'prosemirror-view'
-import { tokenize, SugarHigh } from 'sugar-high/core'
-
+import { SugarHigh, tokenize } from 'sugar-high/core'
+import { lang, languages } from 'sugar-high/lang'
 import type { Parser } from './types'
+
 
 export type { Parser }
 
 const types = SugarHigh.TokenTypes
 
 export function createParser(): Parser {
-  return function parser({ content, pos }) {
+  return function parser({ content, pos, language }) {
     const decorations: Decoration[] = []
 
-    const tokens = tokenize(content)
+    const languageId = language == null ? undefined : lang(language )
+    const languageConfig = languageId && languages.find(lang => lang.id === languageId)?.config
+
+
+    const tokens = tokenize(content, languageConfig )
 
     let from = pos + 1
 
